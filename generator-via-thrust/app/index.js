ec68f5f9ee79888575c0318feab9063bfd6f77d4
Sammy Griffiths <sammy@viacreative.co.uk>
2014-07-07 16:21:26 +0100

    Latest development

--- generator-via-thrust/app/index.js
+++ generator-via-thrust/app/index.js
@@ -34,29 +34,58 @@ var ViaThrustGenerator = yeoman.generators.Base.extend({
         //Pass the answers to the prompt method
         this.prompt(prompts, function (props) {
             this.siteName = props.siteName;
-            this.pages = props.pages.split(',');
             this.responsive = props.responsive;
-            this.structuresStrings = props.structures.replace(/ /g,'').split(',');
-            this.structures = [];
-            for(var i = 0; i < this.structuresStrings.length; i++){
-                this.structures.push(this.structuresStrings[i].split('|')); 
+
+            this.pageStrings = props.pages.replace(/, /g,',').split(',');
+            this.pages = {};
+
+            //Populate the pages object with friendly and slug versions of each page name
+            for(var i=0; i<this.pageStrings.length; i++){
+                this.pages['page_'+i] = [this.pageStrings[i].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}), this._.slugify(this.pageStrings[i])];
             }
 
+            //Convert structures syntaxt to lower case
+            props.structures = props.structures.toLowerCase();
+            //Remove spaces and split it by the commas into an array
+            this.structuresStrings = props.structures.replace(/ /g,'').split(',');
+            this.structuresArray = [];
+            this.structures = {};
+            //Loop through the split structures
+            for(var i = 0; i < this.structuresStrings.length; i++){
+                //Add each argument in the special syntax (split by |) to the structures array
+                this.structuresArray.push(this.structuresStrings[i].split('|')); 
+
+                //If it doesn't already exist, create an entry in the structures object with the page name as the key
+                if(!(this.structuresArray[i][0] in this.structures)){
+                    this.structures[this.structuresArray[i][0]] = {};
+                }
+                //Add each section of page to the right page in the object, with the name as the key and the structure as the value e.g:
+                //home: {slider: 'zeus'}
+                this.structures[this.structuresArray[i][0]][this.structuresArray[i][1]] = this.structuresArray[i][2];
+            }
    
             done();
         }.bind(this));
+    
 
     },
 
-    // parseVaribles: function(){
-    //     var pages = this.pages.split(',');
-    //     var structures = this.structures.split(',');
-    // },
+    availableStructures: function(){
+        this.availableStructures = {
+            zeus: ['article'],
+            poseidon_left: ['aside', 'article'],
+            poseidon_right: ['article', 'aside'],
+            poseidon: ['article', 'article'],
+            hermes_left: ['aside', 'figure', 'figure'],
+            hermes_right: ['figure', 'figure', 'aside'],
+            hermes: ['figure', 'figure', 'figure'],
+            athena: ['figure', 'figure', 'figure', 'figure'],
+        }
+    },
 
     scaffoldFolders: function(){
-        for (var i = 0; i < this.pages.length; i++) {
-            var pageName = this._.slugify(this.pages[i]);
-            this.mkdir('assets/scss/partials/'+pageName);
+        for (var i = 0; i < Object.keys(this.pages).length; i++) {
+            this.mkdir('assets/scss/partials/'+this.pages['page_'+i][1]);
         }
         this.mkdir('assets/scss/config');
         this.directory('assets/scss/partials/global');
@@ -80,10 +109,11 @@ var ViaThrustGenerator = yeoman.generators.Base.extend({
         this.template('_index.html', 'index.html');
         this.template('_bower.json', 'bower.json');
         this.template('assets/scss/config/_settings.scss', 'assets/scss/config/_settings.scss');
-        for (var i = 0; i < this.pages.length; i++) {
-            var pageName = this._.slugify(this.pages[i]);
+        for (var i = 0; i < Object.keys(this.pages).length; i++) {
+            var pageName = this.pages['page_'+i][1];
             var context = {
-                pageName: pageName
+                pageName: pageName,
+                structures: this.structures,
             };
             this.template('_partial.scss', 'assets/scss/partials/'+pageName+'/_'+pageName+'.scss', context);
             this.template('_partial-init.scss', 'assets/scss/partials/'+pageName+'/_init.scss', context);
@@ -91,9 +121,11 @@ var ViaThrustGenerator = yeoman.generators.Base.extend({
         this.template('_partials-init.scss', 'assets/scss/partials/_init.scss');
     },
 
-    // installDependencies: function(){
-    //     this.spawnCommand('bower', ['install']);
-    // }
+    installDependencies: function(){
+        this.spawnCommand('bower', ['install']);
+        this.copy('assets/js/vendor/modernizr-2.7.1.min.js', 'assets/js/vendor/modernizr-2.7.1.min.js');
+    },
+
 });
 
 module.exports = ViaThrustGenerator;
